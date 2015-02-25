4b94244578871bf753ccdd1f34047309521dd3fd
iver56 <iver56@hotmail.com>
2015-02-25 15:17:01 +0100

    Add apiUrl constant and reformat code in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,107 +1,109 @@
 angular.module('useatApp', ['ionic', 'useatApp.controllers', 'useatApp.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($httpProvider) {
-  //Enable cross domain calls
-  $httpProvider.defaults.useXDomain = true;
-
-  //Remove the header used to identify ajax call  that would prevent CORS from working
-  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
+  .constant('apiUrl', 'http://localhost:8000')
 
-  .state('tab.findRoom', {
-    url: '/findRoom',
-    views: {
-      'tab-findRoom': {
-        templateUrl: 'templates/tab-findRoom.html',
-        controller: 'FindRoomCtrl'
-      }
-    }
+  .config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+
+    //Remove the header used to identify ajax call  that would prevent CORS from working
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
 
-  .state('tab.map', {
-      url: '/map',
-      views: {
-        'tab-map': {
-          templateUrl: 'templates/tab-map.html',
-          controller: 'MapCtrl'
-        }
-      }
-    })
-    /*
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-    */
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  .state('tab.favorites', {
-      url: '/favorites',
-      views: {
-        'tab-favorites': {
-          templateUrl: 'templates/tab-favorites.html',
-          controller: 'FavoritesCtrl'
-        }
-      }
-    })
-    /*
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-    */
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('tab.settings', {
-    url: '/settings',
-    views: {
-      'tab-settings': {
-        templateUrl: 'templates/tab-settings.html',
-        controller: 'SettingsCtrl'
-      }
-    }
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.findRoom', {
+        url: '/findRoom',
+        views: {
+          'tab-findRoom': {
+            templateUrl: 'templates/tab-findRoom.html',
+            controller: 'FindRoomCtrl'
+          }
+        }
+      })
+
+      .state('tab.map', {
+        url: '/map',
+        views: {
+          'tab-map': {
+            templateUrl: 'templates/tab-map.html',
+            controller: 'MapCtrl'
+          }
+        }
+      })
+      /*
+       .state('tab.chat-detail', {
+       url: '/chats/:chatId',
+       views: {
+       'tab-chats': {
+       templateUrl: 'templates/chat-detail.html',
+       controller: 'ChatDetailCtrl'
+       }
+       }
+       })
+       */
+
+      .state('tab.favorites', {
+        url: '/favorites',
+        views: {
+          'tab-favorites': {
+            templateUrl: 'templates/tab-favorites.html',
+            controller: 'FavoritesCtrl'
+          }
+        }
+      })
+      /*
+       .state('tab.friend-detail', {
+       url: '/friend/:friendId',
+       views: {
+       'tab-friends': {
+       templateUrl: 'templates/friend-detail.html',
+       controller: 'FriendDetailCtrl'
+       }
+       }
+       })
+       */
+
+      .state('tab.settings', {
+        url: '/settings',
+        views: {
+          'tab-settings': {
+            templateUrl: 'templates/tab-settings.html',
+            controller: 'SettingsCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/findRoom');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/findRoom');
-
-});
