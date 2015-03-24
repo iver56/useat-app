569794107748f6514297069ffb90e0d749e0a528
hannahv <hannahunnesviken@gmail.com>
2015-03-24 16:34:24 +0100

    Filter ting

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,8 +14,11 @@ angular.module('useatApp.controllers', [])
       GeolocationService.getCurrentPosition().then(function(currentPosition) {
         $scope.state = 'LOADING_ROOMS';
 
+        console.log($scope.capacity)
+
         var url = apiUrl + "/rooms/?lat=" + currentPosition.coords.latitude
-          + "&lon=" + currentPosition.coords.longitude;
+          + "&lon=" + currentPosition.coords.longitude
+          + "&min_capacity=" + $scope.capacityList[$scope.capacity];
 
         $http.get(url)
           .success(function(data) {
@@ -46,6 +49,7 @@ angular.module('useatApp.controllers', [])
     };
     $scope.closeModal = function() {
       $scope.modal.hide();
+      $scope.getRooms();
     };
     //Cleanup the modal when we're done with it!
     $scope.$on('$destroy', function() {
