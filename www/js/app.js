95294e882233a0703fbd655b42818320532757a8
iver56 <iver56@hotmail.com>
2015-04-08 15:38:52 +0200

    Hackily fix tab positions on IE

--- www/js/app.js
+++ www/js/app.js
@@ -9,14 +9,10 @@ angular.module(
 )
   .run(function($ionicPlatform, $rootScope, $location) {
     $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
+      if (ionic.Platform.ua.toLowerCase().indexOf('trident') > -1) {
+        setTimeout(function() {
+          document.body.className += ' platform-ie';
+        }, 10);
       }
     });
 
